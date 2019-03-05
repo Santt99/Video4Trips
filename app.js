87085fe572e80c4d98a8440450ae6f7008c140ba
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2019-03-05 08:35:34 -0600

    WebSockets1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const server = require("http").Server(app)
 const expressLayouts = require('express-ejs-layouts')
+const io = require('socket.io')(server);
 
 //Variables
 const PORT = process.env.PORT || 80 
@@ -12,5 +13,10 @@ app.set('view engine', 'ejs')
 
 //Routes
 app.use('/',require('./routes/index'))
+
+// sockets 
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
 
 server.listen(PORT,console.log(`Server Listening to PORT ${PORT}`))
