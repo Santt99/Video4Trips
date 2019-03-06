91e22468c539e5f54c3794238e80a8c688640fb5
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2019-03-05 21:30:25 -0600

    Delete files from files directory

--- routes/upload.js
+++ routes/upload.js
@@ -2,10 +2,25 @@ const express = require('express')
 const router = express.Router()
 const multer  = require('multer')
 const upload = multer({ dest: __dirname + '/../files' })
+const fs = require('fs')
+
+ 
 router.post('/', upload.array('files'), function (req, res, next) {
-    console.log(req.files)
+    let files = req.files
+    
+    deleteFilesFromDirectory(__dirname + '/../files/')
     res.write("Done")
     res.end()
 })
 
+function deleteFilesFromDirectory(path){
+    fs.readdir(path, (err, files) => {
+        if (err) throw err;
+        for (const file of files) {
+          fs.unlink(path + file, err => {
+            if (err) throw err;
+          })
+        }
+    })   
+}
 module.exports = router
